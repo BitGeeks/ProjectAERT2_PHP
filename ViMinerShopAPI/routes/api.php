5a2928c8251d48a5caf75a7a709e7173ab95c343
lesongvi <vi@maxmines.com>
2022-04-01 13:41:48 +0700

    update

--- ViMinerShopAPI/routes/api.php
+++ ViMinerShopAPI/routes/api.php
@@ -23,6 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('users/authenticate', [ApiController::class, 'authenticate']);
 Route::post('users/register', [ApiController::class, 'register']);
 
+Route::get("slideimages", [SlideImagesController::class, 'get_slide_images']);
+
 Route::group(['middleware' => ['jwt.verify']], function() {
     Route::post('users/{id}', [ApiController::class, 'get_user_by_id']);
     Route::put('users/update',  [ApiController::class, 'update_user']);
@@ -30,16 +32,17 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::get('users/records/all',  [ApiController::class, 'get_user_records']);
     Route::get('users/records/count',  [ApiController::class, 'get_user_records_count']);
     Route::post('users/register/validate',  [ApiController::class, 'validate_user']);
-    Route::post("/users/referrals/create", [ApiController::class, 'create_referral_code']);
-    Route::get("/users/referrals/all", [ApiController::class, 'list_all_referral']);
-    Route::get("/users/stats/point", [ApiController::class, 'get_user_stats_points']);
-    Route::get("/users/soldoutnotify", [ApiController::class, 'set_new_product_notify']);
+    Route::post("users/referrals/create", [ApiController::class, 'create_referral_code']);
+    Route::get("users/referrals/all", [ApiController::class, 'list_all_referral']);
+    Route::get("users/stats/point", [ApiController::class, 'get_user_stats_points']);
+    Route::get("users/soldoutnotify", [ApiController::class, 'set_new_product_notify']);
 
     Route::get("useraddresses", [UserAddressController::class, 'get_user_address']);
     Route::get("useraddresses/{id}", [UserAddressController::class, 'get_user_address_by_id']);
     Route::put("useraddresses/update", [UserAddressController::class, 'update_user_address']);
     Route::get("useraddresses", [UserAddressController::class, 'get_user_address']);
-    Route::post("setdefault", [UserAddressController::class, 'set_default']);
+    Route::post("useraddresses/setdefault", [UserAddressController::class, 'set_default']);
 
-    Route::get("slideimages", [SlideImagesController::class, 'get_slide_images']);
+    Route::get("shippingmethods/all", [ShippingMethodsController::class, 'get_shipping_method_list']);
+    Route::get("shippingmethods/flag/{flag}", [ShippingMethodsController::class, 'get_shipping_method_by_flag']);
 });
