7fa2aa95c6eeae7aa72fa9c22b0bb39db4fd2a4d
lesongvi <vi@maxmines.com>
2022-04-01 13:57:16 +0700

    update

--- ViMinerShopAPI/routes/api.php
+++ ViMinerShopAPI/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\UserAddressController;
+use App\Http\Controllers\ShippingMethodsController;
 
 /*
 |--------------------------------------------------------------------------
